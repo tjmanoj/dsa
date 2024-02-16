9e8258f06d6d6613157ddc3551112f255802e698
Manoj T <100189789+tjmanoj@users.noreply.github.com>
2024-02-16 22:29:45 +0530

    Update 004 longest common substring.cpp

--- 004 longest common substring.cpp
+++ 004 longest common substring.cpp
@@ -16,12 +16,44 @@ string longestCommonPrefix(string s[], int n){
     return answer;
 }
 
+// Time complexity :    O(n logn * m)       where m is the max length of stirngs
+//                             +
+//                  O(min(s[0].size() , s[n-1].size()))
+
+
+
+string method2(string s[], int n){ // without sorting
+    // keeping first string constant and compare all string and update lenght
+    string first = s[0];
+    int answer = first.size();
+
+    for(int i=1;i<n;i++){
+        int j=0;
+        string curr = s[i];
+        for(auto i:first)
+            if(i == curr[j]){
+                j++;
+            }
+            else{
+                answer = min(answer, j);
+                break;
+            }
+    }
+    return first.substr(0,answer);
+
+}
+
+// Time Complexity  : O(n * m) 
+// Space Complexity : O(1) 
+
+
+
 
 int main() {
-    string s[] = {"apple", "app", "aptitude"};
+    string s[] = {"apple", "app", "apptitude","appp"};
     int n = sizeof(s) / sizeof(s[0]);
     
-    cout << longestCommonPrefix(s, n);
-
+    // cout << longestCommonPrefix(s, n);
+    cout << method2(s, n);
 
 }
