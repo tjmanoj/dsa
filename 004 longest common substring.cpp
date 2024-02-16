b328dbdf8dc4fa407f44932ae9224b1ce56edf78
Manoj T <100189789+tjmanoj@users.noreply.github.com>
2024-02-16 21:52:48 +0530

    Create 004 longest common substring.cpp

---
+++ 004 longest common substring.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+string longestCommonPrefix(string s[], int n){
+    string answer = "";
+    sort(s, s + n);
+    string first = s[0];
+    string last = s[n-1];
+
+    // check for common str 
+    for(int i=0;i< first.size();i++)
+        if(first[i] == last[i])
+            answer += first[i];
+    
+    return answer;
+}
+
+
+int main() {
+    string s[] = {"apple", "app", "aptitude"};
+    int n = sizeof(s) / sizeof(s[0]);
+    
+    cout << longestCommonPrefix(s, n);
+
+
+}
