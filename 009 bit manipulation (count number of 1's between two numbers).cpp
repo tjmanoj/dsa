9de3104d14f702b370a43abb2b5c6ad126f3676d
Manoj T <100189789+tjmanoj@users.noreply.github.com>
2024-02-29 12:31:42 +0530

    optimized

--- 009 bit manipulation (count number of 1's between two numbers).cpp
+++ 009 bit manipulation (count number of 1's between two numbers).cpp
@@ -13,3 +13,28 @@ int main()
     cout << count;
     return 0;
 }
+
+
+
+// without builtin function
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a=10;
+    int b= 15;
+    int count = 0;
+    for(int i=a; i<= 15;i++){
+        int temp = 0;
+        int s = i;
+        while(s > 0){
+            temp += (1 & s);
+            s = s >> 1;
+        }
+        count += temp;
+    }
+    cout << count;
+    return 0;
+}
