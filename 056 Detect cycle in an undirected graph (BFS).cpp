b363278730ccf6b0e402f7824ceb1066864bb96d
Manoj T <100189789+tjmanoj@users.noreply.github.com>
2024-04-20 22:05:39 +0530

    Create 056 Detect cycle in an undirected graph (BFS).cpp

---
+++ 056 Detect cycle in an undirected graph (BFS).cpp
@@ -0,0 +1,38 @@
+//TC  : O(2n + 2e)  => O(n+e)
+//SC  : O(2n)  visited and queue
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class solution{
+public :
+    bool bfs(int i,vector<vector<int>> &adj, vector<int> &visited){
+        queue<pair<int,int>> q;
+        visited[i] = 1;
+        q.push({i,-1});     // initially no parent so -1
+        while(!q.empty()){
+            int cur = q.front().first;
+            int parent = q.front().second;
+            q.pop();
+            for(auto k: adj[cur]){
+                if(k != parent){
+                if(visited[k]) return true;     //if already visited then cycle
+                q.push({k,cur});
+                visited[k] = 1;
+            }
+            }
+        }
+        return false;
+        
+    }
+    bool checkCycle(int n,vector<vector<int>>& adj) {
+        vector<int> visited(n,0);
+        for(int i=0;i<n;i++){                                   //O(n)
+            if(!visited[i]){
+                if(bfs(i,adj,visited)) return true;             //O(n + 2e) for each bjs
+            }
+        }
+        return false;
+        
+    }
+};
