2d7073f24fdbe86e000cb29e06e807c17c03db9a
Manoj T <100189789+tjmanoj@users.noreply.github.com>
2024-04-27 20:51:24 +0530

    Create 067 Bipartite graph DFS.cpp

---
+++ 067 Bipartite graph DFS.cpp
@@ -0,0 +1,30 @@
+//TC  : O(MN)
+//SC  : O(mn) + O(mn)    auxilary space + stack space
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class solution{
+public :
+    bool dfs(int i, vector<vector<int>> &adj, vector<int> &visited,int color){
+        visited[i] = color;
+        
+        for(auto k: adj[i]){
+            if(visited[k] == -1){
+                if(!dfs(k,adj,visited,1-color)) return false;
+            }
+            else{
+                if(visited[k] == visited[i]) return false;
+            }
+        }
+        return true;
+    }
+    bool bipartite(vector<vector<int>>& adj, int n) {
+        vector<int> visited(n,-1);
+        for(int i=0;i<n;i++){
+            if(visited[i] == -1) if(!dfs(i,adj,visited,0)) return false;
+        }
+        return true;
+        
+    }
+};
