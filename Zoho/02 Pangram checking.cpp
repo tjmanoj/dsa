cca34ef7878c71acf1d9507a487b2cc517cc5697
Manoj T <100189789+tjmanoj@users.noreply.github.com>
2024-05-12 23:37:11 +0530

    Create 02 Pangram checking.cpp

---
+++ Zoho/02 Pangram checking.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    string s;
+    int hash[26] ={0};
+    getline(cin,s);
+
+    for(int i=0;i<s.size();i++){
+        if(s[i] < 97){
+            int ind = (s[i] + 32) - 'a';
+            hash[ind] = 1;
+        }
+        else if(s[i]!= ' '){
+            hash[s[i] -'a'] = 1;
+        }
+    }
+    
+    int flag = 1;
+
+    for(auto i : hash){
+        if(i == 0){
+            flag = 0;
+            break;
+        }
+    }
+
+    if(flag) cout << "Yes";
+    else cout <<"No";
+}
