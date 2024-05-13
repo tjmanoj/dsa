0221ded6a7868535e1ee1dcc87224fc966ed0925
Manoj T <100189789+tjmanoj@users.noreply.github.com>
2024-05-13 14:19:54 +0530

    Update 085 Subarray sum with k sum (positive).cpp

--- 085 Subarray sum with k sum (positive).cpp
+++ 085 Subarray sum with k sum (positive).cpp
@@ -4,22 +4,6 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-
-// int longsubarray(int a[], int n, int k){
-//     int sum = 0;
-//     int max = 0;
-//     map<int, int> m;
-//     for(int i=0;i<n;i++){
-//         sum += a[i];
-//         if(sum == k) max = i+1;
-//         if(m.find(sum - k) != m.end()){
-//             int len = i - m[sum - k];
-//             if(max < len) max = len;
-//         }
-//         if(m.find(sum) == m.end()) m[sum] = i;
-//     }
-//     return max;
-// }
 int window(int a[], int n, int k){
     int i=0,j=0,sum=a[0];
     int max = 0;
