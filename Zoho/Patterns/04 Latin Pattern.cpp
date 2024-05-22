735bb7c36e8daf09d11eaf7f1bfce163c60fe836
Manoj T <100189789+tjmanoj@users.noreply.github.com>
2024-05-22 13:59:08 +0530

    Create 04 Latin Pattern.cpp

---
+++ Zoho/Patterns/04 Latin Pattern.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+// Original Code 
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    for(int i=65;i<n+65;i++){
+        int range = n+65-i;
+        for(int j=0;j<range;j++) cout << (char)(i+j) << ' ';      // first half of each line
+
+        for(int j=65;j<i;j++) cout << (char)j << ' ';             // second half of each line
+
+        cout << endl;
+
+    }
+}
+
+// A B C 
+// B C A 
+// C A B
+
+
+
+// Concept Code  - Latin Pattern
+
+// n = 3 
+// 0 1 2
+// 1 2 0            all the diagonals are same latin pattern
+// 2 0 1
+
+int main(){
+    int n;
+    cin >> n;
+
+    for(int i=0;i<n;i++){
+        int range = n-i;
+        for(int j=0;j<range;j++) cout << i+j << ' ';      // first half
+
+        for(int j=0;j<i;j++) cout << j << ' ';            // second half
+
+        cout << endl;
+
+    }
+}
