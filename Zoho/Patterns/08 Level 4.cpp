b48838211dc06165b870a1dc24d6172555d2a6b7
Manoj T <100189789+tjmanoj@users.noreply.github.com>
2024-05-22 16:47:54 +0530

    Create 08 Level 4.cpp

---
+++ Zoho/Patterns/08 Level 4.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int col = n*2 -1;
+    
+    for(int i=0;i<n;i++){
+        for(int j=0;j<col;j++){
+            if(i+j >= n-1 && i >= j+1-n) cout << 'X';
+            else cout << ' ';
+        }
+    
+        cout << endl;
+    }
+    return 0;
+}
+
+
+//     X    
+//    XXX   
+//   XXXXX  
+//  XXXXXXX 
+// XXXXXXXXX
+
+//Break downs
+
+//     X    
+//    X     
+//   X             i+j == n-1
+//  X       
+// X
+
+    // X    
+    //  X   
+    //   X          i == j-n+1
+    //    X 
+    //     X
