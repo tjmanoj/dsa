2f84c4ab2796e32502ae15d9eba285b40a1c76e4
Manoj T <100189789+tjmanoj@users.noreply.github.com>
2024-05-22 11:55:29 +0530

    Create 01 Number Pattern.cpp

---
+++ Zoho/Patterns/01 Number Pattern.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+   int n;
+   cin >> n;
+   
+   for(int i=0;i< 2*n-1;i++){
+    for(int j=0;j<2*n-1;j++){
+        int top = i;
+        int left = j;
+        int right = (2*n-1) - j;
+        int bottom = (2*n-1) - i;
+        cout << n- min(min(top,left),min(right,bottom));
+    }
+    cout << endl;
+   }
+
+}
+
+// 4444444
+// 4333334
+// 4322234
+// 4321234
+// 4322234
+// 4333334
+// 4444444
