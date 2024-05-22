e5ee365011839d2c671ef5f034b30981aaf2c254
Manoj T <100189789+tjmanoj@users.noreply.github.com>
2024-05-22 15:54:19 +0530

    Create 07 Level 3.cpp

---
+++ Zoho/Patterns/07 Level 3.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(i+j >= n-1) cout << 'X';
+            else cout << ' ';
+        }
+    
+        cout << endl;
+    }
+    return 0;
+}
+//     X
+//    XX
+//   XXX                if(i+j >= n-1) cout << 'X';
+//  XXXX
+// XXXXX
+
+
+// X    
+// XX   
+// XXX                if(i >= j) cout << 'X';
+// XXXX 
+// XXXXX
