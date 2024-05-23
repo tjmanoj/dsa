ec8c49f610330595f0e1d0b6fb9cac15b9b59e53
Manoj T <100189789+tjmanoj@users.noreply.github.com>
2024-05-23 15:02:43 +0530

    Update 01 Number Pattern.cpp

--- Zoho/Patterns/01 Number Pattern.cpp
+++ Zoho/Patterns/01 Number Pattern.cpp
@@ -9,8 +9,8 @@ int main(){
     for(int j=0;j<2*n-1;j++){
         int top = i;
         int left = j;
-        int right = (2*n-1) - j;
-        int bottom = (2*n-1) - i;
+        int right = (2*n-2) - j;
+        int bottom = (2*n-2) - i;
         cout << n- min(min(top,left),min(right,bottom));
     }
     cout << endl;
