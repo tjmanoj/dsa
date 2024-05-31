6ee5264612a20a925f622f78e616799bcb9b4f6f
Manoj T <100189789+tjmanoj@users.noreply.github.com>
2024-05-31 22:47:05 +0530

    Update 08 Level 4.cpp

--- Zoho/Patterns/08 Level 4.cpp
+++ Zoho/Patterns/08 Level 4.cpp
@@ -8,7 +8,7 @@ int main()
     
     for(int i=0;i<n;i++){
         for(int j=0;j<col;j++){
-            if(i+j >= n-1 && i >= j+1-n) cout << 'X';
+            if(i+j >= n-1 && i >= j-n+1) cout << 'X';
             else cout << ' ';
         }
     
