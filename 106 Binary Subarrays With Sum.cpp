1ac8543d0548d1f4ae4313b48dcf76671eaf879c
Manoj T <100189789+tjmanoj@users.noreply.github.com>
2024-06-07 01:23:24 +0530

    Create 106 Binary Subarrays With Sum.cpp

---
+++ 106 Binary Subarrays With Sum.cpp
@@ -0,0 +1,46 @@
+//Fist method using the Map (count of sub array sum with k sum)
+
+class Solution {
+public:
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        int count = 0,sum = 0;
+        map<int,int>m;
+        int l = 0,r = 0;
+        while(r < nums.size()){
+            sum += nums[r];
+
+            if(sum == goal) count++;
+            if(m.find(sum - goal) != m.end()){
+                count += m[sum - goal];
+            }
+
+            m[sum]++;
+            r++;
+
+        }
+        return count;
+    }
+};
+
+
+// Optimal Using the count of (sum <=k ) subarray problem
+
+class Solution {
+public:
+    int func(vector<int>& nums, int goal){    // sum <= k method counting
+        int l=0, r=0,sum = 0,count = 0;
+        if(goal < 0) return 0;
+        while(r < nums.size()){
+            sum += nums[r];
+
+            while(sum > goal) sum -= nums[l++];     // use the while loop for counting problems if longest asked then use  "if"
+
+            if(sum <= goal) count += (r-l+1);
+            r++;
+        }
+        return count;
+    }
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        return func(nums,goal) - func(nums,goal-1);
+    }
+};
