38d744942e0b7c1204105498ec81a72f638967c6
Manoj T <100189789+tjmanoj@users.noreply.github.com>
2024-06-07 14:08:42 +0530

    Update 108 Count Number of Nice Subarrays.cpp

--- 108 Count Number of Nice Subarrays.cpp
+++ 108 Count Number of Nice Subarrays.cpp
@@ -1,3 +1,31 @@
+// Method 1
+class Solution {
+public:
+    int helper(vector<int>& nums, int goal){    // sum <= k method counting
+        int l=0, r=0,sum = 0,count = 0;
+        if(goal < 0) return 0;
+        while(r < nums.size()){
+            sum += nums[r]%2;
+
+            while(sum > goal) sum -= nums[l++]%2;     // use the while loop for counting problems if longest asked then use  "if"
+
+            if(sum <= goal) count += (r-l+1);
+            r++;
+        }
+        return count;
+    }
+    int numberOfSubarrays(vector<int>& nums, int k) {
+        return helper(nums,k) - helper(nums,k-1);
+    }
+};
+
+
+
+
+
+
+
+
 // TC  : O(2N)
 // SC  : O(1)
 
