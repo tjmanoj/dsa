f970eea52face4a290e714bc9725fd56470f7aef
Manoj T <100189789+tjmanoj@users.noreply.github.com>
2024-06-15 17:44:57 +0530

    Update random number.cpp

--- Zoho/Custom STL Libraries/random number.cpp
+++ Zoho/Custom STL Libraries/random number.cpp
@@ -3,8 +3,8 @@ using namespace std;
 
 
 int main(){
-    srand(time(nullptr));
-    cout << rand() % 10;
+    srand(time(nullptr));      // ithu use panlana same value return panite irukum
+    cout << rand() % 10;       // rand function 0 -> rand_max kulla ethachi oru number generate panum athuku modulo pota antha range kulla kidaikum.
     
 
 }
