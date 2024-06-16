5b1add7d1446c9bb121599161339976a65ceccd1
Manoj T <100189789+tjmanoj@users.noreply.github.com>
2024-06-16 23:12:46 +0530

    Update 04 Matrix Rotation.cpp

--- Zoho/Matrix/04 Matrix Rotation.cpp
+++ Zoho/Matrix/04 Matrix Rotation.cpp
@@ -16,3 +16,31 @@ public:
         for(int i=0;i<n;i++) reverse(matrix[i].begin(),matrix[i].end());
     }
 };
+
+
+
+// Without reverse function
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        for(int i=0;i<n;i++){
+            int l = 0,r = n-1;
+            while(l < r){
+                int temp =  matrix[i][l];
+                matrix[i][l] = matrix[i][r];
+                matrix[i][r] = temp;
+                l++;
+                r--;
+            }
+        }
+    }
+};
