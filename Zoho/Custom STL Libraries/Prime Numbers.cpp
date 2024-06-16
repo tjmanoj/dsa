228ded4dd3c937fc771c2e1d5f87ca8209fab964
Manoj T <100189789+tjmanoj@users.noreply.github.com>
2024-06-16 13:30:36 +0530

    Create Prime Numbers.cpp

---
+++ Zoho/Custom STL Libraries/Prime Numbers.cpp
@@ -0,0 +1,23 @@
+// TC : O(N) + O(N log(N))
+// SC : O(N)
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n = 25;
+    int prime[n+1] = {0};
+    for(int i=2;i*i<=n;i++){                 // O(N log(log N))
+            if(!prime[i]){
+                for(int j=i*i;j<=n;j+=i){
+                    prime[j] = 1;
+                }
+            }
+    }
+    
+    for(int i=2;i<=n;i++){                    // O(N)
+        if(!prime[i]) cout << i << " ";
+    }
+}
+
+
