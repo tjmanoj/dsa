38a9d68ce704423d9bb88be65ed51540a10d651f
Manoj T <100189789+tjmanoj@users.noreply.github.com>
2024-06-22 22:39:56 +0530

    Create 08 Search in Rotated Sorted Array.cpp

---
+++ Binary Search/08 Search in Rotated Sorted Array.cpp
@@ -0,0 +1,33 @@
+// TC : O(log n)
+// SC : O(1)
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int low = 0;
+        int high = nums.size() - 1;
+
+        while(low <= high){
+            int mid = low + (high - low)/2;
+
+            if(nums[mid] == target) return mid;
+
+            // if left part is sorted
+            if(nums[low] <= nums[mid]){                             
+                if(target >= nums[low] && target <= nums[mid]){
+                    high = mid - 1;                                // if element exits in this range
+                }
+                else low = mid+1;                                  // if element not exists
+            }
+
+            // if right part is sorted
+            else{
+                if(target >= nums[mid] && target <= nums[high]){
+                    low = mid + 1;
+                }
+                else high = mid - 1;
+            }
+        }
+        return -1;
+    }
+};
