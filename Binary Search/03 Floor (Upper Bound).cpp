c07ddd78b6fc54564344c641396a624b79717a7d
Manoj T <100189789+tjmanoj@users.noreply.github.com>
2024-06-22 17:23:34 +0530

    Create 03 Floor (Upper Bound).cpp

---
+++ Binary Search/03 Floor (Upper Bound).cpp
@@ -0,0 +1,29 @@
+// TC : O(log n)
+// SC : O(1)
+
+class Solution{
+  public:
+    // Function to find floor of x
+    // n: size of vector
+    // x: element whose floor is to find
+    int findFloor(vector<long long> v, long long n, long long x){
+        
+        // here we need to find <= which is floor of the given value  (Upper Bound)
+        
+        long long low = 0;
+        long long high = n-1;
+        
+        long long ans = -1;
+        
+        while(low <= high){
+            long long mid = low + (high - low)/2;
+            
+            if(v[mid] <= x){
+                ans = mid;
+                low = mid + 1;
+            }
+            else high = mid -1;
+        }
+        return ans;
+    }
+};
