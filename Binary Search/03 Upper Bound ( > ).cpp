da684da01a6ae30057308f4b5c04f60cd289307b
Manoj T <100189789+tjmanoj@users.noreply.github.com>
2024-06-22 17:29:13 +0530

    Create 03 Upper Bound ( > ).cpp

---
+++ Binary Search/03 Upper Bound ( > ).cpp
@@ -0,0 +1,15 @@
+int upper_bound(int a[], int n, int x){
+    int low = 0;
+    int high = n - 1;
+    int ans = n;
+    while(low <= high){
+        int mid = low + (high - low) / 2;
+        if(a[mid] > x){
+            ans = mid;
+            high = mid - 1;
+        }
+        else 
+            low = mid + 1;
+    }
+    return ans;
+}
