4afe236a67701426a746893530009ac5c652ab71
Manoj T <100189789+tjmanoj@users.noreply.github.com>
2024-06-23 16:59:43 +0530

    Update 12 Single element in an sorted array.cpp

--- Binary Search/12 Single element in an sorted array.cpp
+++ Binary Search/12 Single element in an sorted array.cpp
@@ -7,8 +7,8 @@ public:
         if(nums.size() == 1 || nums[0] != nums[1]) return nums[0];
         if(nums[n-1] != nums[n-2]) return nums[n-1];
 
-        int low = 0;
-        int high = n - 1;
+        int low = 1;
+        int high = n - 2;
         while(low <= high){
             int mid = low + (high - low)/2;
 
