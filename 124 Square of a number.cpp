9e72c68e18656ea9250994955aeaef1b15d8d2d5
Manoj T <100189789+tjmanoj@users.noreply.github.com>
2024-06-25 19:58:53 +0530

    Create 124 Square of a number.cpp

---
+++ 124 Square of a number.cpp
@@ -0,0 +1,22 @@
+// TC : O(log n)
+// SC : O(1)
+
+class Solution{
+  public:
+    long long int floorSqrt(long long int x) 
+    {
+        int ans = 1;
+        long long low = 1,high = x;
+        
+        while(low <= high){
+            long long mid = low + (high - low)/2;
+            
+            if(mid * mid <= x){
+                ans = mid;
+                low = mid+1;
+            }
+            else high = mid - 1;
+        }
+        return ans;
+    }
+};
