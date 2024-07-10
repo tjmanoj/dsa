84d87c3e4e4d4eadec8ba697f876380b18eb86b5
Manoj T <100189789+tjmanoj@users.noreply.github.com>
2024-07-11 01:51:00 +0530

    Create 154 Permutations with Spaces.cpp

---
+++ 154 Permutations with Spaces.cpp
@@ -0,0 +1,23 @@
+class Solution {
+  public:
+    void helper(string s,vector<string>& res,int ind,string cur){
+        if(ind == s.size()){
+            res.push_back(cur);
+            return;
+        }
+        
+        //Include
+        helper(s,res,ind+1,cur+ " " +s[ind]);
+        
+        //Exclude
+        helper(s,res,ind+1,cur + s[ind]);
+        
+    }
+    vector<string> permutation(string s) {
+        vector<string> res;
+        string cur ="";
+        cur += s[0];    // 'A'
+        helper(s,res,1,cur);
+        return res;
+    }
+};
