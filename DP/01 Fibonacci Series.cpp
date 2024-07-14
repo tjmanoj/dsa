a813ac85f5b4aa40959609be17ca3d008b648136
Manoj T <100189789+tjmanoj@users.noreply.github.com>
2024-07-15 00:01:56 +0530

    Create 01 Fibonacci Series.cpp

---
+++ DP/01 Fibonacci Series.cpp
@@ -0,0 +1,18 @@
+// Memoization
+// TC  : O(N)
+// SC  : O(N) + O(N)            stack space + array space
+class Solution {
+public:
+    int fibo(int n,vector<int> &dp){
+        if(n <= 1) return n;
+
+        if(dp[n] != -1) return dp[n];
+
+        return dp[n] = fibo(n-1,dp) + fibo(n-2,dp);
+    }
+
+    int fib(int n) {
+        vector<int> dp(n+1,-1);
+        return fibo(n,dp);
+    }
+};
