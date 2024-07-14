1792ebc20db0e74954e51e06efb376dc558f71b0
Manoj T <100189789+tjmanoj@users.noreply.github.com>
2024-07-15 00:03:09 +0530

    Update 01 Fibonacci Series.cpp

--- DP/01 Fibonacci Series.cpp
+++ DP/01 Fibonacci Series.cpp
@@ -6,13 +6,13 @@ public:
     int fibo(int n,vector<int> &dp){
         if(n <= 1) return n;
 
-        if(dp[n] != -1) return dp[n];
+        if(dp[n] != -1) return dp[n];                    // step 2
 
-        return dp[n] = fibo(n-1,dp) + fibo(n-2,dp);
+        return dp[n] = fibo(n-1,dp) + fibo(n-2,dp);      // step 3
     }
 
     int fib(int n) {
-        vector<int> dp(n+1,-1);
-        return fibo(n,dp);
+        vector<int> dp(n+1,-1);                         // step 1
+        return fibo(n,dp); 
     }
 };
