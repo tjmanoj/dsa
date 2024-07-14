404c7cd91b0ab0b3f1ae77655a003a5189f61473
Manoj T <100189789+tjmanoj@users.noreply.github.com>
2024-07-14 22:30:11 +0530

    Update 059 Maximum sum of hour glass.cpp

--- 059 Maximum sum of hour glass.cpp
+++ 059 Maximum sum of hour glass.cpp
@@ -11,7 +11,7 @@ class Solution {
         if(n < 3) return -1;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(i-1 >=0 && i+1 < n && j-1 >=0 && j+1 < m){
+                if(i > 0 && i+1 < n && j > 0 && j+1 < m){
                     int sum = mat[i][j] + mat[i-1][j] + mat[i+1][j] + mat[i-1][j-1] + mat[i-1][j+1] + mat[i+1][j+1] + mat[i+1][j-1];
                     ans = max(ans,sum);
                 }
