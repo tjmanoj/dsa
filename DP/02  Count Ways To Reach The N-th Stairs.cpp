584835ba1aca617e5a9d913e38c2adeea35d8155
Manoj T <100189789+tjmanoj@users.noreply.github.com>
2024-07-15 23:57:44 +0530

    Update 02  Count Ways To Reach The N-th Stairs.cpp

--- DP/02  Count Ways To Reach The N-th Stairs.cpp
+++ DP/02  Count Ways To Reach The N-th Stairs.cpp
@@ -12,8 +12,41 @@ int func(int n,vector<int> &dp){
 
     return dp[n] = (func(n-1,dp) + func(n-2,dp)) % MOD;
 }
-int countDistinctWays(int nStairs) {
-    vector<int> dp(nStairs+1,-1);
-    return func(nStairs,dp);
 
+
+// Tabulation
+
+// TC : O(N)
+// SC : O(N)
+int func(int n){
+    if(n == 0) return 1;        // only one chance
+    if(n == 1) return 1;        // since it is a edge case f(n-2) will be -1
+
+    vector<int> dp(n+1,-1);
+    dp[0] = 1,dp[1] = 1;
+
+    for(int i=2;i<=n;i++) dp[i] = (dp[i-1] + dp[i-2]) % MOD;
+
+    return dp[n];
 }
+
+// Space Optimized
+
+// TC : O(N)
+// SC : O(1)
+int func(int n){
+    if(n == 0) return 1;
+    if(n == 1) return 1;
+    int cur;
+    int prev2 = 1, prev = 1;
+
+    for(int i=2;i<=n;i++){
+        cur = (prev + prev2) % MOD;
+        prev2 = prev;
+        prev = cur;
+    }
+     
+    return prev;
+}
+
+
