9dec4914fdb8f1f4de0f9dc1998edcf931f6a1f1
Manoj T <100189789+tjmanoj@users.noreply.github.com>
2024-07-24 13:24:40 +0530

    Update 135 Valid Parenthesis String.cpp

--- 135 Valid Parenthesis String.cpp
+++ 135 Valid Parenthesis String.cpp
@@ -25,3 +25,43 @@ public:
     }
 
 };
+
+
+//Using stack
+// TC : O(N)
+// SC : O(N)
+
+class Solution {
+public:
+    bool helper(stack<int> brackets,stack<int> stars){
+        while(!brackets.empty()){
+            if(stars.empty()) return false;
+
+            else if(stars.top() > brackets.top()){
+                brackets.pop();
+                stars.pop();
+            }
+            else return false;
+        }
+        return true;
+    }
+    bool checkValidString(string s) {
+        stack<int> brackets;
+        stack<int> stars;
+
+        for(int i=0;i<s.size();i++){
+            if(s[i] == '(') brackets.push(i);
+
+            else if(s[i] == '*') stars.push(i);
+
+            else{                                                   // closing bracket
+
+                if(!brackets.empty()) brackets.pop();               // if opening bracket available in brackets stack pop from it
+                else if(!stars.empty()) stars.pop();                     // if opening bracket available in stars stack pop from it
+                else return false;                                  // if both are empty return false(cannot balance)
+            }
+        }
+
+        return helper(brackets,stars);                              // after for loop balancing remaining elements in both stack
+    }
+};
