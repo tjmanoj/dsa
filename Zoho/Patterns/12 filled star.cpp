e3d3b3b84df2b83a43f6cd6233842b3f1cac14f7
Manoj T <100189789+tjmanoj@users.noreply.github.com>
2025-09-14 11:33:16 +0530

    Update 12 filled star.cpp

--- Zoho/Patterns/12 filled star.cpp
+++ Zoho/Patterns/12 filled star.cpp
@@ -1,3 +1,21 @@
+//java code
+public class Main {
+    public static void main(String[] args) {
+      int n = 7;
+      
+      for(int i=0; i<n; i++)
+      {
+        for(int j=0; j<n*2-1;j++)
+        {
+          if(i+j >= n/2 && i >= (j-n/2) && i <= (j+n/2) && i+j <= (3*n-2)/2) System.out.print('X');
+          else System.out.print(' ');
+        }
+        System.out.println();
+      }
+      
+    }
+}
+
 #include <iostream>
 using namespace std;
 int main()
